6312f12813c74d098f47f83d437472e49cf38ce9
huangjiali-git <2401754616@qq.com>
2024-12-14 01:47:34 +0800

    反对v反对v反对

---
+++ snake-game/src/test/java/com/haha/snakegame/SnakeGameApplicationTests.java
@@ -0,0 +1,17 @@
+package com.haha.snakegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+
+}
